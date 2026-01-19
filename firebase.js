7bdc958b44dc75878245141c5da7eab33c125105
Marstorekita <likezelwit@gmail.com>
2026-01-19 20:56:07 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,20 @@
 // firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.x/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.x/firebase-database.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDXYDqFmhO8nacuX-hVnNsXMmpeqwYlW7U",
   authDomain: "wifist-d3588.firebaseapp.com",
   projectId: "wifist-d3588",
   storageBucket: "wifist-d3588.firebasestorage.app",
   messagingSenderId: "460842291436",
-  appId: "1:460842291436:web:f82e6f0c7fc668fc72d5c9"
+  appId: "1:460842291436:web:f82e6f0c7fc668fc72d5c9",
+  databaseURL: "https://wifist-d3588-default-rtdb.firebaseio.com" // Tambahkan ini!
 };
 
-// Inisialisasi Firebase (cek agar tidak inisialisasi dua kali)
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
+// Inisialisasi
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
 
-// Global variable agar bisa dipakai di script lain
-const db = firebase.firestore();
+// Export agar bisa dipakai di Checkout & Konfirmasi
+export { db };
