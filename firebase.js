fcb5c39ec62d98c3a81fb3ab41817f48fc6ba17d
Marstorekita <likezelwit@gmail.com>
2026-01-19 21:03:17 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,15 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.x/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.x/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.12/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.10.12/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDXYDqFmhO8nacuX-hVnNsXMmpeqwYlW7U",
-  authDomain: "wifist-d3588.firebaseapp.com",
-  projectId: "wifist-d3588",
-  storageBucket: "wifist-d3588.firebasestorage.app",
-  messagingSenderId: "460842291436",
-  appId: "1:460842291436:web:f82e6f0c7fc668fc72d5c9",
-  databaseURL: "https://wifist-d3588-default-rtdb.firebaseio.com" // Tambahkan ini!
+    apiKey: "AIzaSyDXYDqFmhO8nacuX-hVnNsXMmpeqwYlW7U",
+    authDomain: "wifist-d3588.firebaseapp.com",
+    databaseURL: "https://wifist-d3588-default-rtdb.firebaseio.com",
+    projectId: "wifist-d3588",
+    storageBucket: "wifist-d3588.firebasestorage.app",
+    messagingSenderId: "460842291436",
+    appId: "1:460842291436:web:f82e6f0c7fc668fc72d5c9"
 };
 
-// Inisialisasi
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-// Export agar bisa dipakai di Checkout & Konfirmasi
-export { db };
+export const db = getDatabase(app);
